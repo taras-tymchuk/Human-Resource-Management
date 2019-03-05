450e11c1db143baf117102e3672c5b5b1b68c9e2
taras-tymchuk <tymakruk@gmail.com>
2019-03-06 00:50:57 +0200

    Added Addresses table

---
+++ HRMDatabase/dbo/Tables/Addresses.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[Addresses]
+(
+	[AddressId] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [Country] VARCHAR(30) NOT NULL, 
+    [Residence] VARCHAR(30) NOT NULL, 
+    [StreetName] VARCHAR(50) NOT NULL, 
+    [BuildingNumber] INT NOT NULL
+)
