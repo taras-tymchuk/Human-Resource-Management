00be98f666a19f26136104bea20566cccdaa2a6a
taras-tymchuk <tymakruk@gmail.com>
2019-03-06 01:03:29 +0200

    Added JobAssignment table

---
+++ HRMDatabase/dbo/Tables/JobAssignment.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE [dbo].[JobAssignment]
+(
+	[AssignmentId] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [EmployeeId] INT NOT NULL, 
+    [Position] VARCHAR(50) NOT NULL, 
+    [Salary] MONEY NOT NULL, 
+    [DepartmentId] INT NOT NULL, 
+    [SupervisorId] INT NULL, 
+    [DateStarted] DATE NOT NULL, 
+    [AdditionId] INT NULL
+)
