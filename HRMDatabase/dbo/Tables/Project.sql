fc2fc8f3c44693d892a17faa9d21430171a75f19
taras-tymchuk <tymakruk@gmail.com>
2019-03-06 00:56:49 +0200

    Added Project table

---
+++ HRMDatabase/dbo/Tables/Project.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[Project]
+(
+	[ProjectId] INT NOT NULL PRIMARY KEY, 
+    [Title] VARCHAR(50) NOT NULL, 
+    [Customer] VARCHAR(50) NOT NULL, 
+    [PMId] INT NOT NULL, 
+    [Budget] MONEY NOT NULL, 
+    [Description] TEXT NULL
+)
